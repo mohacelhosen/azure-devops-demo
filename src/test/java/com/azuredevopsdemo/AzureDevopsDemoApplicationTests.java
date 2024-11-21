a6b5dd7eb85fb5f87f89efd1b5ff2f74a5f5c7c7
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2024-11-21 19:16:44 +0600

    azure demo

---
+++ src/test/java/com/azuredevopsdemo/AzureDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuredevopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
